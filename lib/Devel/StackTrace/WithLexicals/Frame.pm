5a57af5db72173fc0683eeb88c68197edfa474f6
Shawn M Moore <cpan@sartak.org>
2008-08-07 03:38:06 +0000

    Import of SARTAK/Devel-StackTrace-WithLexicals-0.01 from CPAN.
    gitpan-cpan-distribution: Devel-StackTrace-WithLexicals
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         SARTAK/Devel-StackTrace-WithLexicals-0.01.tar.gz
    gitpan-cpan-author:       SARTAK
    gitpan-cpan-maturity:     released

---
+++ lib/Devel/StackTrace/WithLexicals/Frame.pm
@@ -0,0 +1,27 @@
+#!/usr/bin/env perl
+package Devel::StackTrace::WithLexicals::Frame;
+use strict;
+use warnings;
+use Devel::StackTrace;
+use base 'Devel::StackTraceFrame';
+
+sub new {
+    my $class = shift;
+    my %args  = @_;
+
+    my $self = $class->SUPER::new(
+        $args{caller},
+        $args{args},
+        $args{respect_overload},
+        $args{max_arg_length},
+    );
+
+    $self->{lexicals} = $args{lexicals};
+
+    return $self;
+}
+
+sub lexicals { shift->{lexicals} }
+
+1;
+
